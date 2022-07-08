10fd0489fcab01249864911551653e92fb13ae00
ruhulsmart786 <90688372+ruhulsmart786@users.noreply.github.com>
2022-07-09 01:29:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,444 @@
+const _0x4ea7cb = _0x2f7e;
+(function (_0x3e3205, _0x1e1f40) {
+  const _0x45e786 = _0x2f7e,
+    _0x4f2492 = _0x3e3205();
+  while (!![]) {
+    try {
+      const _0x128612 =
+        parseInt(_0x45e786(0x166)) / 0x1 +
+        -parseInt(_0x45e786(0x18c)) / 0x2 +
+        parseInt(_0x45e786(0x182)) / 0x3 +
+        (-parseInt(_0x45e786(0x198)) / 0x4) *
+          (parseInt(_0x45e786(0x155)) / 0x5) +
+        parseInt(_0x45e786(0x186)) / 0x6 +
+        -parseInt(_0x45e786(0x18d)) / 0x7 +
+        (-parseInt(_0x45e786(0x199)) / 0x8) *
+          (-parseInt(_0x45e786(0x194)) / 0x9);
+      if (_0x128612 === _0x1e1f40) break;
+      else _0x4f2492["push"](_0x4f2492["shift"]());
+    } catch (_0x597e45) {
+      _0x4f2492["push"](_0x4f2492["shift"]());
+    }
+  }
+})(_0x2d13, 0xa43d3);
+function _0x2d13() {
+  const _0x34fbda = [
+    "movie",
+    "</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20",
+    "form",
+    "Adventure",
+    "name",
+    "scrollIntoView",
+    "</span>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20",
+    "innerText",
+    "prev",
+    "main",
+    "Music",
+    "join",
+    "orange",
+    "tag",
+    "next",
+    "json",
+    "then",
+    "\x22\x20alt=\x22",
+    "386601EdFuYX",
+    "innerHTML",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<br/>\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<button\x20class=\x22know-more\x22\x20id=\x22",
+    "click",
+    "768132ZdnPaP",
+    "\x22\x20title=\x22",
+    "Fantasy",
+    "http://via.placeholder.com/1080x1580",
+    "War",
+    "disabled",
+    "831596AaQViU",
+    "7881538dteMku",
+    "Romance",
+    "YouTube",
+    "remove",
+    "value",
+    "&with_genres=",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<iframe\x20width=\x22560\x22\x20height=\x22315\x22\x20src=\x22https://www.youtube.com/embed/",
+    "4410504ijcNAi",
+    "Documentary",
+    "active",
+    "Animation",
+    "1264hFfpNa",
+    "32PHRUeW",
+    "style",
+    "Clear\x20x",
+    "split",
+    "search",
+    "createElement",
+    "Drama",
+    "/movie/",
+    "append",
+    "add",
+    "/videos?",
+    "</span>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22overview\x22>\x0a\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<h3>Overview</h3>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20",
+    "div",
+    "TV\x20Movie",
+    "&query=",
+    "splice",
+    "show",
+    "Mystery",
+    "hide",
+    "log",
+    "getElementById",
+    "includes",
+    "api_key=1cf50e6248dc270629e802686245c2c8",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20<h1\x20class=\x22no-results\x22>",
+    "appendChild",
+    "<h1\x20class=\x22no-results\x22>No\x20Results\x20Found</h1>",
+    "page",
+    "smooth",
+    "current",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20<br/>\x0a\x0a\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22dots\x22>",
+    "Crime",
+    "13445pdflCq",
+    "Horror",
+    "querySelectorAll",
+    "History",
+    "/search/movie?",
+    "length",
+    ".embed",
+    "addEventListener",
+    "</h1>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<br/>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20",
+    "forEach",
+    "results",
+    "classList",
+    "toString",
+    "Action",
+    "myNav",
+    "\x22>\x0a\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22movie-info\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<h3>",
+    "width",
+    "847066fJhkfN",
+    "right-arrow",
+    "original_title",
+    "/discover/movie?sort_by=popularity.desc&",
+    "green",
+    "highlight",
+    "submit",
+    "push",
+    ".dot",
+    "\x22\x20class=\x22embed\x20hide\x22\x20frameborder=\x220\x22\x20allow=\x22accelerometer;\x20autoplay;\x20clipboard-write;\x20encrypted-media;\x20gyroscope;\x20picture-in-picture\x22\x20allowfullscreen></iframe>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20",
+  ];
+  _0x2d13 = function () {
+    return _0x34fbda;
+  };
+  return _0x2d13();
+}
+const API_KEY = _0x4ea7cb(0x1af),
+  BASE_URL = "https://api.themoviedb.org/3",
+  API_URL = BASE_URL + _0x4ea7cb(0x169) + API_KEY,
+  IMG_URL = "https://image.tmdb.org/t/p/w500",
+  searchURL = BASE_URL + _0x4ea7cb(0x159) + API_KEY,
+  genres = [
+    { id: 0x1c, name: _0x4ea7cb(0x162) },
+    { id: 0xc, name: _0x4ea7cb(0x173) },
+    { id: 0x10, name: _0x4ea7cb(0x197) },
+    { id: 0x23, name: "Comedy" },
+    { id: 0x50, name: _0x4ea7cb(0x154) },
+    { id: 0x63, name: _0x4ea7cb(0x195) },
+    { id: 0x12, name: _0x4ea7cb(0x19f) },
+    { id: 0x29ff, name: "Family" },
+    { id: 0xe, name: _0x4ea7cb(0x188) },
+    { id: 0x24, name: _0x4ea7cb(0x158) },
+    { id: 0x1b, name: _0x4ea7cb(0x156) },
+    { id: 0x28a2, name: _0x4ea7cb(0x17a) },
+    { id: 0x25b0, name: _0x4ea7cb(0x1aa) },
+    { id: 0x29fd, name: _0x4ea7cb(0x18e) },
+    { id: 0x36e, name: "Science\x20Fiction" },
+    { id: 0x2a12, name: _0x4ea7cb(0x1a6) },
+    { id: 0x35, name: "Thriller" },
+    { id: 0x2a00, name: _0x4ea7cb(0x18a) },
+    { id: 0x25, name: "Western" },
+  ],
+  main = document[_0x4ea7cb(0x1ad)](_0x4ea7cb(0x179)),
+  form = document[_0x4ea7cb(0x1ad)](_0x4ea7cb(0x172)),
+  search = document[_0x4ea7cb(0x1ad)](_0x4ea7cb(0x19d)),
+  tagsEl = document[_0x4ea7cb(0x1ad)]("tags"),
+  prev = document[_0x4ea7cb(0x1ad)](_0x4ea7cb(0x178)),
+  next = document[_0x4ea7cb(0x1ad)](_0x4ea7cb(0x17e)),
+  current = document[_0x4ea7cb(0x1ad)](_0x4ea7cb(0x152));
+var currentPage = 0x1,
+  nextPage = 0x2,
+  prevPage = 0x3,
+  lastUrl = "",
+  totalPages = 0x64,
+  selectedGenre = [];
+setGenre();
+function setGenre() {
+  const _0x32044c = _0x4ea7cb;
+  (tagsEl[_0x32044c(0x183)] = ""),
+    genres[_0x32044c(0x15e)]((_0x373fb7) => {
+      const _0x39ae6e = _0x32044c,
+        _0x1b4255 = document[_0x39ae6e(0x19e)](_0x39ae6e(0x1a5));
+      _0x1b4255["classList"][_0x39ae6e(0x1a2)](_0x39ae6e(0x17d)),
+        (_0x1b4255["id"] = _0x373fb7["id"]),
+        (_0x1b4255[_0x39ae6e(0x177)] = _0x373fb7[_0x39ae6e(0x174)]),
+        _0x1b4255[_0x39ae6e(0x15c)]("click", () => {
+          const _0x4c724c = _0x39ae6e;
+          selectedGenre[_0x4c724c(0x15a)] == 0x0
+            ? selectedGenre["push"](_0x373fb7["id"])
+            : selectedGenre[_0x4c724c(0x1ae)](_0x373fb7["id"])
+            ? selectedGenre["forEach"]((_0xb9a89d, _0x6b30a9) => {
+                const _0x55074d = _0x4c724c;
+                _0xb9a89d == _0x373fb7["id"] &&
+                  selectedGenre[_0x55074d(0x1a8)](_0x6b30a9, 0x1);
+              })
+            : selectedGenre[_0x4c724c(0x16d)](_0x373fb7["id"]),
+            console["log"](selectedGenre),
+            getMovies(
+              API_URL +
+                _0x4c724c(0x192) +
+                encodeURI(selectedGenre[_0x4c724c(0x17b)](","))
+            ),
+            highlightSelection();
+        }),
+        tagsEl[_0x39ae6e(0x1a1)](_0x1b4255);
+    });
+}
+function highlightSelection() {
+  const _0x3d7326 = _0x4ea7cb,
+    _0x71b93e = document["querySelectorAll"](".tag");
+  _0x71b93e["forEach"]((_0x389a17) => {
+    const _0x2aece8 = _0x2f7e;
+    _0x389a17[_0x2aece8(0x160)]["remove"](_0x2aece8(0x16b));
+  }),
+    clearBtn(),
+    selectedGenre[_0x3d7326(0x15a)] != 0x0 &&
+      selectedGenre["forEach"]((_0x134411) => {
+        const _0x3bb4b3 = _0x3d7326,
+          _0x40bab1 = document["getElementById"](_0x134411);
+        _0x40bab1[_0x3bb4b3(0x160)][_0x3bb4b3(0x1a2)](_0x3bb4b3(0x16b));
+      });
+}
+function clearBtn() {
+  const _0x8ed2bb = _0x4ea7cb;
+  let _0x3bc816 = document[_0x8ed2bb(0x1ad)]("clear");
+  if (_0x3bc816) _0x3bc816["classList"]["add"](_0x8ed2bb(0x16b));
+  else {
+    let _0x42fdd2 = document[_0x8ed2bb(0x19e)]("div");
+    _0x42fdd2[_0x8ed2bb(0x160)][_0x8ed2bb(0x1a2)](
+      _0x8ed2bb(0x17d),
+      _0x8ed2bb(0x16b)
+    ),
+      (_0x42fdd2["id"] = "clear"),
+      (_0x42fdd2[_0x8ed2bb(0x177)] = _0x8ed2bb(0x19b)),
+      _0x42fdd2[_0x8ed2bb(0x15c)](_0x8ed2bb(0x185), () => {
+        (selectedGenre = []), setGenre(), getMovies(API_URL);
+      }),
+      tagsEl[_0x8ed2bb(0x1a1)](_0x42fdd2);
+  }
+}
+getMovies(API_URL);
+function getMovies(_0x38724f) {
+  const _0x15361e = _0x4ea7cb;
+  (lastUrl = _0x38724f),
+    fetch(_0x38724f)
+      [_0x15361e(0x180)]((_0x561485) => _0x561485["json"]())
+      [_0x15361e(0x180)]((_0x19858d) => {
+        const _0x15e496 = _0x15361e;
+        console[_0x15e496(0x1ac)](_0x19858d["results"]);
+        if (_0x19858d[_0x15e496(0x15f)]["length"] !== 0x0) {
+          showMovies(_0x19858d[_0x15e496(0x15f)]),
+            (currentPage = _0x19858d[_0x15e496(0x1b3)]),
+            (nextPage = currentPage + 0x1),
+            (prevPage = currentPage - 0x1),
+            (totalPages = _0x19858d["total_pages"]),
+            (current[_0x15e496(0x177)] = currentPage);
+          if (currentPage <= 0x1)
+            prev[_0x15e496(0x160)][_0x15e496(0x1a2)](_0x15e496(0x18b)),
+              next[_0x15e496(0x160)][_0x15e496(0x190)](_0x15e496(0x18b));
+          else
+            currentPage >= totalPages
+              ? (prev["classList"][_0x15e496(0x190)](_0x15e496(0x18b)),
+                next[_0x15e496(0x160)][_0x15e496(0x1a2)](_0x15e496(0x18b)))
+              : (prev[_0x15e496(0x160)]["remove"](_0x15e496(0x18b)),
+                next["classList"][_0x15e496(0x190)](_0x15e496(0x18b)));
+          tagsEl[_0x15e496(0x175)]({ behavior: _0x15e496(0x1b4) });
+        } else main[_0x15e496(0x183)] = _0x15e496(0x1b2);
+      });
+}
+function showMovies(_0x236290) {
+  const _0x42980f = _0x4ea7cb;
+  (main[_0x42980f(0x183)] = ""),
+    _0x236290[_0x42980f(0x15e)]((_0x35b5a9) => {
+      const _0x236f82 = _0x42980f,
+        {
+          title: _0x35c8ac,
+          poster_path: _0x2b3026,
+          vote_average: _0x61d366,
+          overview: _0x26a555,
+          id: _0x1e1ff5,
+        } = _0x35b5a9,
+        _0x465b88 = document[_0x236f82(0x19e)](_0x236f82(0x1a5));
+      _0x465b88[_0x236f82(0x160)][_0x236f82(0x1a2)](_0x236f82(0x170)),
+        (_0x465b88[_0x236f82(0x183)] =
+          "\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<img\x20src=\x22" +
+          (_0x2b3026 ? IMG_URL + _0x2b3026 : _0x236f82(0x189)) +
+          _0x236f82(0x181) +
+          _0x35c8ac +
+          _0x236f82(0x164) +
+          _0x35c8ac +
+          "</h3>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<span\x20class=\x22" +
+          getColor(_0x61d366) +
+          "\x22>" +
+          _0x61d366 +
+          _0x236f82(0x1a4) +
+          _0x26a555 +
+          _0x236f82(0x184) +
+          _0x1e1ff5 +
+          "\x22>Know\x20More</button\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20"),
+        main[_0x236f82(0x1b1)](_0x465b88),
+        document[_0x236f82(0x1ad)](_0x1e1ff5)[_0x236f82(0x15c)](
+          _0x236f82(0x185),
+          () => {
+            console["log"](_0x1e1ff5), openNav(_0x35b5a9);
+          }
+        );
+    });
+}
+const overlayContent = document["getElementById"]("overlay-content");
+function openNav(_0x277e36) {
+  const _0xc53d13 = _0x4ea7cb;
+  let _0x3728bf = _0x277e36["id"];
+  fetch(BASE_URL + _0xc53d13(0x1a0) + _0x3728bf + _0xc53d13(0x1a3) + API_KEY)
+    ["then"]((_0xf4a06f) => _0xf4a06f[_0xc53d13(0x17f)]())
+    ["then"]((_0x103ac4) => {
+      const _0x28e0ba = _0xc53d13;
+      console[_0x28e0ba(0x1ac)](_0x103ac4);
+      if (_0x103ac4) {
+        document["getElementById"]("myNav")[_0x28e0ba(0x19a)][
+          _0x28e0ba(0x165)
+        ] = "100%";
+        if (_0x103ac4[_0x28e0ba(0x15f)][_0x28e0ba(0x15a)] > 0x0) {
+          var _0x3feeef = [],
+            _0x12376e = [];
+          _0x103ac4[_0x28e0ba(0x15f)][_0x28e0ba(0x15e)](
+            (_0x28d6a7, _0x1a819a) => {
+              const _0x497e62 = _0x28e0ba;
+              let {
+                name: _0x2baab2,
+                key: _0xc6ffa2,
+                site: _0x37ccda,
+              } = _0x28d6a7;
+              _0x37ccda == _0x497e62(0x18f) &&
+                (_0x3feeef[_0x497e62(0x16d)](
+                  _0x497e62(0x193) +
+                    _0xc6ffa2 +
+                    _0x497e62(0x187) +
+                    _0x2baab2 +
+                    _0x497e62(0x16f)
+                ),
+                _0x12376e[_0x497e62(0x16d)](
+                  "\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<span\x20class=\x22dot\x22>" +
+                    (_0x1a819a + 0x1) +
+                    _0x497e62(0x176)
+                ));
+            }
+          );
+          var _0x31a4b0 =
+            _0x28e0ba(0x1b0) +
+            _0x277e36[_0x28e0ba(0x168)] +
+            _0x28e0ba(0x15d) +
+            _0x3feeef[_0x28e0ba(0x17b)]("") +
+            _0x28e0ba(0x153) +
+            _0x12376e[_0x28e0ba(0x17b)]("") +
+            _0x28e0ba(0x171);
+          (overlayContent[_0x28e0ba(0x183)] = _0x31a4b0),
+            (activeSlide = 0x0),
+            showVideos();
+        } else overlayContent[_0x28e0ba(0x183)] = _0x28e0ba(0x1b2);
+      }
+    });
+}
+function closeNav() {
+  const _0x4ace75 = _0x4ea7cb;
+  document[_0x4ace75(0x1ad)](_0x4ace75(0x163))[_0x4ace75(0x19a)][
+    _0x4ace75(0x165)
+  ] = "0%";
+}
+var activeSlide = 0x0,
+  totalVideos = 0x0;
+function showVideos() {
+  const _0x527b61 = _0x4ea7cb;
+  let _0x46d4d5 = document[_0x527b61(0x157)](_0x527b61(0x15b)),
+    _0x37c8fa = document["querySelectorAll"](_0x527b61(0x16e));
+  (totalVideos = _0x46d4d5["length"]),
+    _0x46d4d5[_0x527b61(0x15e)]((_0x381a51, _0x12c20e) => {
+      const _0x1208df = _0x527b61;
+      activeSlide == _0x12c20e
+        ? (_0x381a51[_0x1208df(0x160)][_0x1208df(0x1a2)](_0x1208df(0x1a9)),
+          _0x381a51[_0x1208df(0x160)][_0x1208df(0x190)](_0x1208df(0x1ab)))
+        : (_0x381a51[_0x1208df(0x160)][_0x1208df(0x1a2)](_0x1208df(0x1ab)),
+          _0x381a51[_0x1208df(0x160)]["remove"](_0x1208df(0x1a9)));
+    }),
+    _0x37c8fa[_0x527b61(0x15e)]((_0x328519, _0x5e5662) => {
+      const _0x34b66a = _0x527b61;
+      activeSlide == _0x5e5662
+        ? _0x328519[_0x34b66a(0x160)][_0x34b66a(0x1a2)]("active")
+        : _0x328519["classList"][_0x34b66a(0x190)](_0x34b66a(0x196));
+    });
+}
+const leftArrow = document[_0x4ea7cb(0x1ad)]("left-arrow"),
+  rightArrow = document[_0x4ea7cb(0x1ad)](_0x4ea7cb(0x167));
+leftArrow["addEventListener"](_0x4ea7cb(0x185), () => {
+  activeSlide > 0x0 ? activeSlide-- : (activeSlide = totalVideos - 0x1),
+    showVideos();
+}),
+  rightArrow[_0x4ea7cb(0x15c)](_0x4ea7cb(0x185), () => {
+    activeSlide < totalVideos - 0x1 ? activeSlide++ : (activeSlide = 0x0),
+      showVideos();
+  });
+function getColor(_0x266e7e) {
+  const _0x2996f6 = _0x4ea7cb;
+  if (_0x266e7e >= 0x8) return _0x2996f6(0x16a);
+  else return _0x266e7e >= 0x5 ? _0x2996f6(0x17c) : "red";
+}
+function _0x2f7e(_0x4c0a5f, _0x2115ee) {
+  const _0x2d1300 = _0x2d13();
+  return (
+    (_0x2f7e = function (_0x2f7e4e, _0x1f8dc0) {
+      _0x2f7e4e = _0x2f7e4e - 0x152;
+      let _0x6a52f0 = _0x2d1300[_0x2f7e4e];
+      return _0x6a52f0;
+    }),
+    _0x2f7e(_0x4c0a5f, _0x2115ee)
+  );
+}
+form[_0x4ea7cb(0x15c)](_0x4ea7cb(0x16c), (_0x541e5d) => {
+  const _0x3d2188 = _0x4ea7cb;
+  _0x541e5d["preventDefault"]();
+  const _0x596153 = search[_0x3d2188(0x191)];
+  (selectedGenre = []),
+    setGenre(),
+    _0x596153
+      ? getMovies(searchURL + _0x3d2188(0x1a7) + _0x596153)
+      : getMovies(API_URL);
+}),
+  prev[_0x4ea7cb(0x15c)](_0x4ea7cb(0x185), () => {
+    prevPage > 0x0 && pageCall(prevPage);
+  }),
+  next[_0x4ea7cb(0x15c)](_0x4ea7cb(0x185), () => {
+    nextPage <= totalPages && pageCall(nextPage);
+  });
+function pageCall(_0x4111ab) {
+  const _0x5b2dcb = _0x4ea7cb;
+  let _0x47da8b = lastUrl[_0x5b2dcb(0x19c)]("?"),
+    _0x32eada = _0x47da8b[0x1]["split"]("&"),
+    _0x131d6c =
+      _0x32eada[_0x32eada[_0x5b2dcb(0x15a)] - 0x1][_0x5b2dcb(0x19c)]("=");
+  if (_0x131d6c[0x0] != _0x5b2dcb(0x1b3)) {
+    let _0x2e455b = lastUrl + "&page=" + _0x4111ab;
+    getMovies(_0x2e455b);
+  } else {
+    _0x131d6c[0x1] = _0x4111ab[_0x5b2dcb(0x161)]();
+    let _0x5d134f = _0x131d6c["join"]("=");
+    _0x32eada[_0x32eada[_0x5b2dcb(0x15a)] - 0x1] = _0x5d134f;
+    let _0x2d8c51 = _0x32eada[_0x5b2dcb(0x17b)]("&"),
+      _0x127568 = _0x47da8b[0x0] + "?" + _0x2d8c51;
+    getMovies(_0x127568);
+  }
+}
